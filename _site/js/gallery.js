9366f067820a04376b8fefaf89c10704c8e0007e
Tim Carter <timothy.carter.texan@gmail.com>
2018-09-15 22:56:57 -0500

    Finish the JavaScript gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,30 @@
+// Activates the image Gallery
+// The main task is to attach an event listener to each image in the Gallery
+// and respond appropriately on click
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  let currentClass = "current";
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      let altImageTxt = thumbnail.dataset.title;
+      mainImage.setAttribute("alt", altImageTxt);
+
+      // Change which image is current
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
